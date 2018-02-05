fc3b24d02e4b9e6a73c112d85ca30de981e551dc
jk2489 <jiyoung.kim1721@gmail.com>
2018-02-05 22:23:55 +0900

    Enyo app is added

---
+++ Enyo/draw-circle/index.js
@@ -0,0 +1,23 @@
+// This is the default "main" file, specified from the root package.json file
+// The ready function is excuted when the DOM is ready for usage.
+
+var ready = require('enyo/ready');
+var Control = require('enyo/Control');
+
+var Light = Control.kind({
+	published: {
+		"color": "yellow"
+	},
+	style: "width: 50px; height: 50px; border-radius: 50%",
+	create: function() {
+		this.inherited(arguments);
+		this.colorChanged();
+	},
+	colorChanged: function(oldValue) {
+		this.applyStyle("background-color", this.color);
+	}
+});
+
+ready(function() {
+	new Light().renderInto(document.body);
+});
